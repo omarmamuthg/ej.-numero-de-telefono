c8264671b36e54ba8e2b90b40186bda263eaead3
omarmamuthg <omar.mamuthg@gmail.com>
2024-09-08 19:29:28 -0600

    se agregan unas condiciones dentro del while antes de dar el resultado

--- numero de telefono/Program.cs
+++ numero de telefono/Program.cs
@@ -1,55 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //using System.Numerics;
 //(812) 373 - 3290
-//string[] number_phone = new string[10];
-//Console.WriteLine("Escribe un numero de telefono");
-//string numero_telefono = Console.ReadLine();
 
-//for (int i = 0; i < numero_telefono.Length; i++)
-//{
+using System.Timers;
 
-//    number_phone[i] = numero_telefono[i].ToString();
-//}
-//string cadena1 = "";
-//string cadena2 = "";
-//string cadena3 = "";
-//string cadena_total = "";
-
-//foreach (var cadena in Enumerable.Range(0, number_phone.Length))
-//{
-//    if (cadena >= 0 && cadena <= 2)
-//    {
-//        cadena1 = cadena1 + number_phone[cadena];
-//    }
-
-//    if (cadena >= 3 && cadena <= 5)
-//    {
-//        cadena2 = cadena2 + number_phone[cadena];
-//    }
-
-//    if (cadena >= 6 && cadena <= 9)
-//    {
-//        cadena3 = cadena3 + number_phone[cadena];
-//    }
-
-//}
-
-//Console.Write($"\n({cadena1}) {cadena2}-{cadena3}");
 string[] number_phone = new string[10];
 
-Console.WriteLine("Escribe un numero de telefono");
-string numero_telefono = Console.ReadLine();
+string numero_telefono;
 
-
-for (int i = 0; i < numero_telefono.Length; i++)
+while (true)
 {
+    Console.WriteLine("Escribe un numero de telefono");
+    numero_telefono = Console.ReadLine();
 
-    number_phone[i] = numero_telefono[i].ToString();
+    if (!numero_telefono.All(char.IsDigit))
+    {
+        Console.WriteLine("ingresaste una letra, intenta de nuevo por favor\n");
+        continue;
+    }
+
+    if (numero_telefono.Length < 10)
+    {
+        Console.WriteLine("El numero necesita tener 10 digitos, intenta de nuevo por favor\n");
+        continue;
+    }
+
+    if (numero_telefono.Length > 10)
+    {
+        Console.WriteLine("Solamente se necesitan 10 digitos, intenta de nuevo por favor\n");
+        continue;
+    }
+
+    for (int i = 0; i < numero_telefono.Length; i++)
+    {
+        number_phone[i] = numero_telefono[i].ToString();
+    }
+    break;
 }
-
-string resultado= NumeroTelefono(numero_telefono, number_phone);
-
+string resultado = NumeroTelefono(numero_telefono, number_phone);
 Console.Write($"\nEl numero de telefono es: {resultado}");
+
+
+
+
+
 
 static string NumeroTelefono(string PNumero, string[] number_phone)
 {
